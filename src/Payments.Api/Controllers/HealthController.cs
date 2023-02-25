0378faa5c6140b2013540b7155e3ff8cf451bc98
Kashif Soofi <kashif.soofi+git@gmail.com>
2023-02-25 13:47:11 +0000

    Add health endpoint

---
+++ src/Payments.Api/Controllers/HealthController.cs
@@ -0,0 +1,17 @@
+﻿namespace Payments.Api.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Payments.Contracts.Responses;
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var health = new Health(true);
+            return Ok(health);
+        }
+    }
+}
---
+++ src/Payments.Contracts/Responses/Health.cs
@@ -0,0 +1,12 @@
+﻿namespace Payments.Contracts.Responses
+{
+    public class Health
+    {
+        public Health(bool ok)
+        {
+            Ok = ok;
+        }
+
+        public bool Ok { get; }
+    }
+}
